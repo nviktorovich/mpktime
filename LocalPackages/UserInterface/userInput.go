188f39e06fad442087125689e03884885c0d52df
nmaslenko <nvmaslenko@gmail.com>
2023-02-08 14:12:34 +0500

    убрал закомментированные функции

--- LocalPackages/UserInterface/userInput.go
+++ LocalPackages/UserInterface/userInput.go
@@ -12,24 +12,6 @@ import (
 	"github.com/martinlindhe/inputbox"
 )
 
-// func GetInput() (string, string) {
-
-// 	var hh, mm string
-// 	color.Magenta("'ksa' корректировка времени.\n\n")
-// 	color.Cyan("Введите часы и минуты в формате ЧЧ ММ и нажмите Enter\n")
-// 	fmt.Scanf("%s %s", &hh, &mm)
-
-// 	if checkHours(hh) && checkMinutes(mm) {
-// 		fmt.Println("вы ввели:", hh, mm)
-
-// 	} else {
-// 		fmt.Printf("Введены некоректные данные Часы: %s, Минуты: %s\n", hh, mm)
-// 		time.Sleep(time.Second * 20)
-// 		os.Exit(1)
-// 	}
-// 	return hh, mm
-
-// }
 // checkHours осуществляет проверку валидности введенных часов,
 // возвращает true если проверка пройдена, в противном случае false
 func checkHours(s string) bool {
